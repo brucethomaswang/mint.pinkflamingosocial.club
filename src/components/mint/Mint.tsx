a5c659b63e7f9036cf2b71e1e2cc2c181ac65f23
Samson303 <samson.mulugeta@code.berlin>
2021-11-17 19:29:11 +0000

    feature: mint page update

--- src/components/mint/Mint.tsx
+++ src/components/mint/Mint.tsx
@@ -1,26 +1,43 @@
-import React from 'react'
 import MintButton from '../mintButton/MintButton'
 import MintProgress from '../mintProgress/MintProgress'
 import styles from './Mint.module.scss'
 import ClubSign from '../../assets/pfsc_sign.svg'
+import { useFlamingo } from '../../providers/PinkFlamingoSocialClubProvider'
+import { useEffect, useState } from 'react'
+import RedeemButton from '../RedeemButton/RedeemButton'
 
 const Mint = () => {
+  const { checkIfEligibleForAirdrop } = useFlamingo()
+  const [isEligible, setIsEligible] = useState<boolean>(false)
+
+  useEffect(() => {
+    const data = async () => {
+      const isEligible = await checkIfEligibleForAirdrop()
+      if (isEligible !== undefined) {
+        setIsEligible(isEligible)
+      }
+    }
+
+    data()
+  }, [checkIfEligibleForAirdrop])
+
   return (
     <div className={styles.wrapper}>
-      {/* <div className={styles.title}>
-                Pink Flamingo Social Club
-            </div> */}
       <div>
         <img src={ClubSign} alt="Pink Flamingo Social Club Logo" className={styles.clubSign} />
       </div>
       <div className={styles.description}>
-        The promised lands are burning, the great migration is underway. These new lands offer
+        The promised lands are burning, The Great Migration is underway. These new lands offer
         hope, prosperity and intoxicating indulgence for the traveling flamingo. On the volcanic
-        banks of the Great Pink Lake the <span className={styles.pink}>genesis 777 Flamingos</span> have built a grandiose club to
-        house the elusive, the elite, the sinful and the sinister.
+        banks of the Great Pink Lake a grandiose club is home to the elusive, the elite, the
+        sinful and the sinister. Flamingos from many chains will travel far and wide to unlock
+        the secrets of the infamous Pink Flamingo Social Club.
       </div>
       <MintProgress />
-      <MintButton />
+      <div className={styles.buttons}>
+        <MintButton />
+        {isEligible && <RedeemButton />}
+      </div>
     </div>
   )
 }
