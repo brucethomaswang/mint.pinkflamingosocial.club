19f19cd45cb8c240ee3806eb921b64f25129f15a
brucethomaswang <bruce@bear.financial>
2022-12-05 13:40:35 +1100

    Fix Web3 Provider

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,17 @@ import { FC } from 'react'
 import config from 'config'
 import { MetaMaskProvider } from 'metamask-react'
 import Home from 'pages/home'
-// import Web3Provider from 'providers/Web3Provider'
+import Web3Provider from 'providers/Web3Provider'
 import PinkFlamingoSocialClubProvider from 'providers/PinkFlamingoSocialClubProvider'
 
 const App: FC = () => {
   return (
     <MetaMaskProvider>
-      <PinkFlamingoSocialClubProvider>
-        <Home />
-      </PinkFlamingoSocialClubProvider>
+      <Web3Provider>
+        <PinkFlamingoSocialClubProvider>
+          <Home />
+        </PinkFlamingoSocialClubProvider>
+      </Web3Provider>
     </MetaMaskProvider>
   )
 }
