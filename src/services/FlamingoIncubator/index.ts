4f086cfd761b602cec20d02d5e9b58c564405689
brucethomaswang <bruce@bear.financial>
2023-05-15 19:21:25 +1000

    Tweaks

--- src/services/FlamingoIncubator/index.ts
+++ src/services/FlamingoIncubator/index.ts
@@ -12,9 +12,8 @@ export const functions = {
     const fee = BigNumber.from('4000000000000000')
     const contract = new Contract(config.incubatorAddress, INCUBATOR, signer)
     const feeInWei = fee.mul(tokens.length)
-    const feeInEther = utils.parseEther(feeInWei.toString())
     return contract.incubate(tokens, {
-      value: feeInEther
+      value: feeInWei
     })
   }
 }
