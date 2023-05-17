a929417de8c1b1f1a3a831f76f95c6a7da10ebdf
brucethomaswang <bruce@bear.financial>
2023-05-17 10:58:36 +1000

    Change fee

--- src/services/FlamingoIncubator/index.ts
+++ src/services/FlamingoIncubator/index.ts
@@ -9,7 +9,7 @@ export const contract = new Contract(config.incubatorAddress, INCUBATOR, getDefa
 export const functions = {
   fee: async () => await contract.fee(),
   incubate: async (tokens: number[], signer: any) => {
-    const fee = BigNumber.from('4000000000000000')
+    const fee = BigNumber.from('5500000000000000')
     const contract = new Contract(config.incubatorAddress, INCUBATOR, signer)
     const feeInWei = fee.mul(tokens.length)
     return contract.incubate(tokens, {
